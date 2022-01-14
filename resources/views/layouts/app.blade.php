850d0b15d79c130ad25364d247a542dc88ad8f4d
Marcoseven <marco.caliciotti3@gmail.com>
2022-01-14 16:33:17 +0100

    Added changes
    Created the PageController.php, the Trip.php model, the app.blade.php page and modified the welcome page in home.blade.php and also the route in web.php.

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
+        @yield('custom-css')
+        <title>Trips @yield('title', 'laravel-migration-seeder')</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        @yield('content')
+    </body>
+</html>
