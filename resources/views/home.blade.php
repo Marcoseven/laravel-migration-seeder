3b34758345cdb5bebbf2781d1d989667896ef0e8
Marcoseven <marco.caliciotti3@gmail.com>
2022-01-14 17:09:57 +0100

    Added changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,13 +2,13 @@
 @section('content')
 
 <div class="container">
-    <h1>
+    <h1 class="text-center">
         Pacchetti di viaggio offerti
     </h1>
 
     @forelse($trips as $trip)
         <h4>Località: {{ $trip->locality }}</h4>
-        <h4>Giono di partenza: {{ $trip->departure_date }}</h4>
+        <h4>Giorno di partenza: {{ $trip->departure_date }}</h4>
         <h4>Numero di giorni: {{ $trip->number_of_days }}</h4>
         <hr>
         @empty
